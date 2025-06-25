2d826e2005dfa1cd6c9472ed1d3807f7a4fe6031
SemenK <63603619+Dobrobrav@users.noreply.github.com>
2025-06-25 16:58:07 +0700

    add getting task command + refactoring (#8)

--- app/start_handlers.py
+++ app/start_handlers.py
@@ -1,10 +1,15 @@
 import structlog
+from aiogram import Router
+from aiogram.filters import CommandStart
 from aiogram.types import Message, FSInputFile
 from pathlib import Path
 
 logger = structlog.get_logger(__name__)
 
+start_router = Router()
 
+
+@start_router.message(CommandStart())
 async def welcome(message: Message) -> None:
     logger.info('sending welcome message')
     await message.answer_photo(FSInputFile(Path(__file__).resolve().parent / 'images' / 'kenobi.png'))
